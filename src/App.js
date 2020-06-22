63662efcc7ac7fe28a2f6fafb5990fc260736f6d
unknown <eng.hossamsoliman@gmail.com>
2020-06-22 06:32:30 +0300

    added some changes

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
   render() {
     return (
       <div className=" container">
-        <h1 className="center blue-text">Todo's</h1>
+        <h1 className="center blue-text">Todos</h1>
         <Todos todos={this.state.todos} del={this.deleteTodo} />
         <AddTodo AddItem={this.AddItem} />
       </div>
